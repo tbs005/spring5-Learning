8e7dc88ed887f6a6ba1d13347fbfc3b5e2385bc2
tbs005 <tbs005@qq.com>
2018-11-21 10:38:36 +0800

    java字符串常量池

--- note/src/main/java/com/tbs005/note/javabasics/StringTest.java
+++ note/src/main/java/com/tbs005/note/javabasics/StringTest.java
@@ -2,7 +2,7 @@ package com.tbs005.note.javabasics;
 
 public class StringTest {
     public static void main(String[] args){
-//        Java常量池和对象
+    		//Java字符串常量池
             String a = "aaaabbbbb";
             String b = "aaaa" + "bbbbb";
             String c = new String("aaaabbbbb");
